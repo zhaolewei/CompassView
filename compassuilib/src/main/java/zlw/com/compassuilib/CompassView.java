a150f204dafab219aa9bf35916a1b2dc66fc1cd2
zlw <739043667@qq.com>
2017-07-25 17:03:50 +0800

    兼容屏幕适配

--- compassuilib/src/main/java/zlw/com/compassuilib/CompassView.java
+++ compassuilib/src/main/java/zlw/com/compassuilib/CompassView.java
@@ -7,6 +7,7 @@ import android.graphics.Paint;
 import android.graphics.Rect;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 
@@ -29,16 +30,32 @@ public class CompassView extends View {
 
 
     private float rotate = 0f; //顺时针旋转角度
-    private float textW = 0, textH = 0; //TODO:测量
-    private float textOriW = 0, textOriH = 0; //TODO:测量
-    private float textAngleW = 0; //TODO:测量
+    private float textW = 0, textH = 0;
+    private float textOriW = 0, textOriH = 0;
+    private float textAngleW1, textAngleW2;
 
-    private double lineRateSize = 3d / 66d;
-
-    private static final float DIVIDE_COUNT = 120; //将圆划分为120等份
 
     private int width, height;
 
+    private static final float DIVIDE_COUNT = 120; //将圆划分为120等份
+    private double lineRateSize = 1 / 22d;
+    private int lineColor;
+    private int keyLinecolor;
+    private int mainLinecolor;
+    private int edgeTextColor;
+    private int orientationTextColor;
+    private int angleTextColor;
+
+    private int edgeTextSize;
+    private int orientationTextSize;
+    private int angleTextSize;
+
+    private int oriTextMargin;
+    private int rowPitch;
+    private int mainLineLength;
+
+    private boolean isDebug = true;
+    private int edgeTextMargin;
 
     //外置接口
     public void setRotate(float rotate) {
@@ -62,65 +79,101 @@ public class CompassView extends View {
         init();
     }
 
-
     private void init() {
+        lineColor = Color.parseColor("#00cccc");
+        keyLinecolor = Color.parseColor("#ffffee");
+        mainLinecolor = Color.parseColor("#ffffee");
+        edgeTextColor = Color.parseColor("#00ffff");
+        orientationTextColor = Color.parseColor("#00cccc");
+        angleTextColor = Color.parseColor("#ffffff");
+
+
+//                edgeTextSize = 18;
+//        edgeTextMargin = 30;
+//        orientationTextSize = 20;
+//        oriTextMargin = 35;
+//        angleTextSize = 28;
+//        rowPitch = 26;
+//        mainLineLength = 15;
+
+        //渐进公式
+
+
         mPaint = new Paint();
-        mPaint.setColor(Color.parseColor("#00cccc"));
+        mPaint.setColor(lineColor);
         mPaint.setStrokeWidth(1);
         mPaint.setAntiAlias(true);
         mKeyPaint = new Paint();
         mKeyPaint.setStrokeWidth(1);
-        mKeyPaint.setColor(Color.parseColor("#ffffee"));
+        mKeyPaint.setColor(keyLinecolor);
         mKeyPaint.setAntiAlias(true);
 
         mMainPaint = new Paint();
         mMainPaint.setStrokeWidth(4);
-        mMainPaint.setColor(Color.parseColor("#ffffee"));
+        mMainPaint.setColor(mainLinecolor);
         mMainPaint.setAntiAlias(true);
 
         mTextPaint = new Paint();
-        mTextPaint.setColor(Color.parseColor("#00ffff"));
+        mTextPaint.setColor(edgeTextColor);
         mTextPaint.setStrokeWidth(2);
-        mTextPaint.setTextSize(18);
+        mTextPaint.setTextSize(edgeTextSize);
         mTextPaint.setAntiAlias(true);
 
-        mTmpPaint = new Paint();
-        mTmpPaint.setColor(Color.parseColor("#ff0000"));
-        mTmpPaint.setStrokeWidth(4);
-        mTmpPaint.setAntiAlias(true);
-
+        if (isDebug) {
+            mTmpPaint = new Paint();
+            mTmpPaint.setColor(Color.parseColor("#ff0000"));
+            mTmpPaint.setStrokeWidth(8);
+            mTmpPaint.setAntiAlias(true);
+        }
 
         mOrientationTextPoint = new Paint();
-        mOrientationTextPoint.setColor(Color.parseColor("#00cccc"));
+        mOrientationTextPoint.setColor(orientationTextColor);
         mOrientationTextPoint.setStrokeWidth(3);
-        mOrientationTextPoint.setTextSize(20);
+        mOrientationTextPoint.setTextSize(orientationTextSize);
         mOrientationTextPoint.setAntiAlias(true);
 
         mAngleTextPoint = new Paint();
-        mAngleTextPoint.setColor(Color.parseColor("#ffffff"));
+        mAngleTextPoint.setColor(angleTextColor);
         mAngleTextPoint.setStrokeWidth(2);
-        mAngleTextPoint.setTextSize(28);
+        mAngleTextPoint.setTextSize(angleTextSize);
         mAngleTextPoint.setAntiAlias(true);
 
         mMidpointTextPoint = new Paint();
-        mMidpointTextPoint.setColor(Color.parseColor("#ffffff"));
+        mMidpointTextPoint.setColor(angleTextColor);
         mMidpointTextPoint.setStrokeWidth(2);
-        mMidpointTextPoint.setTextSize(22);
+        mMidpointTextPoint.setTextSize(angleTextSize * 22 / 28);
         mMidpointTextPoint.setAntiAlias(true);
 
         initWH();
     }
 
+    void initSize() {
+        edgeTextSize = 38 - (1080 - width) / 30;
+        edgeTextMargin = 50 - (1080 - width) / 30;
+        orientationTextSize = 42 - (1080 - width) / 30;
+        oriTextMargin = 92 - (1080 - width) / 12;
+        angleTextSize = 60 - (1080 - width) * 4 / 75;
+        rowPitch = 56 - (1080 - width) / 20;
+        mainLineLength = 35 - (1080 - width) / 30;
+    }
+
     private void initWH() {
-        String test = "2700";
+        String test = "120°";
         Rect rect = new Rect();
-        mPaint.getTextBounds(test, 0, test.length(), rect);
+        mTextPaint.getTextBounds(test, 0, test.length(), rect);
         textW = rect.width();//文字宽
         textH = rect.height();//文字高
 
+        test = "120";
         rect = new Rect();
         mAngleTextPoint.getTextBounds(test, 0, test.length(), rect);
-        textAngleW = rect.width();//文字宽
+        textAngleW1 = rect.width();//文字宽
+
+        test = "东北";
+        rect = new Rect();
+        mMidpointTextPoint.getTextBounds(test, 0, test.length(), rect);
+        textAngleW2 = rect.width();//文字宽
+
 
         test = "东";
         rect = new Rect();
@@ -134,6 +187,10 @@ public class CompassView extends View {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         width = getMeasuredWidth();
         height = getMeasuredHeight();
+        Log.d("lwz", "width: " + width + " ;height :" + height);
+        height = width;
+        initSize();
+        init();
     }
 
     @Override
@@ -149,7 +206,8 @@ public class CompassView extends View {
             if ((rotateAngle + rotate) % 30 == 0) {
                 //绘制关键线
                 canvas.drawLine(getRotatePointX(rotateAngle, 75, height / 2), getRotatePointY(rotateAngle, 75, height / 2),
-                        getRotatePointX(rotateAngle, (float) (width * lineRateSize + 80), height / 2), getRotatePointY(rotateAngle, (float) (width * lineRateSize + 80), height / 2), mKeyPaint);
+                        getRotatePointX(rotateAngle, (float) (width * lineRateSize + 80), height / 2),
+                        getRotatePointY(rotateAngle, (float) (width * lineRateSize + 80), height / 2), mKeyPaint);
 
                 //绘制文字
                 float genAngle = (((270 - rotateAngle < 0 ? (270 - rotateAngle + 360) : (270 - rotateAngle)) - rotate) + 360) % 360;//转换起始角度，以最上方为0度
@@ -163,8 +221,10 @@ public class CompassView extends View {
                     strA = "" + (int) genAngle + "°";
 
                 }
-                canvas.drawText(strA, getRotatePointX(rotateAngle, 50, height / 2) - textW / 2, getRotatePointY(rotateAngle, 50, height / 2) + textH / 2, mTextPaint);
-//                canvas.drawPoint(getRotatePointX(rotateAngle, 50, height / 2), getRotatePointY(rotateAngle, 50, height / 2), mTmpPaint);
+                canvas.drawText(strA, getRotatePointX(rotateAngle, 80 - edgeTextMargin, height / 2) - textW / 2, getRotatePointY(rotateAngle, 80 - edgeTextMargin, height / 2) + textH / 2, mTextPaint);
+                if (isDebug) {
+                    canvas.drawPoint(getRotatePointX(rotateAngle, 80 - edgeTextMargin, height / 2), getRotatePointY(rotateAngle, 80 - edgeTextMargin, height / 2), mTmpPaint);
+                }
 
 
                 //绘制东西南北文字
@@ -185,9 +245,11 @@ public class CompassView extends View {
             }
         }
         //绘制度数
-        canvas.drawText((int) Math.abs(rotate) + "°", width / 2 - textAngleW / 2 + 8, height / 2, mAngleTextPoint);
+        canvas.drawText((int) Math.abs(rotate) + "°", width / 2 - textAngleW1 / 2, height / 2, mAngleTextPoint);
 
-        canvas.drawLine(width / 2, 80, width / 2, 120, mMainPaint);
+        canvas.drawLine(width / 2, 80 - mainLineLength,
+                width / 2, (float) (width * lineRateSize + 80),
+                mMainPaint);
 
         //绘制当前方向
         String currentOri;
@@ -209,13 +271,18 @@ public class CompassView extends View {
             currentOri = "西北";
         }
 
-        canvas.drawText(currentOri, width / 2 - textAngleW / 2 + 12, height / 2 + 26, mMidpointTextPoint);
+        canvas.drawText(currentOri, width / 2 - textAngleW2 / 2, height / 2 + rowPitch, mMidpointTextPoint);
+        if (isDebug) {
+            canvas.drawPoint(width / 2, height / 2 + rowPitch / 2, mTmpPaint);
+        }
 
     }
 
     private void drawOriText(Canvas canvas, String strOri, float rotateAngle) {
-        canvas.drawText(strOri, getRotatePointX(rotateAngle, 115, height / 2) - textOriW / 2, getRotatePointY(rotateAngle, 115, height / 2) + textOriH / 2, mOrientationTextPoint);
-//        canvas.drawPoint(getRotatePointX(rotateAngle, 120, height / 2), getRotatePointY(rotateAngle, 120, height / 2), mTmpPaint);
+        canvas.drawText(strOri, getRotatePointX(rotateAngle, 80 + oriTextMargin, height / 2) - textOriW / 2, getRotatePointY(rotateAngle, 80 + oriTextMargin, height / 2) + textOriH / 2, mOrientationTextPoint);
+        if (isDebug) {
+            canvas.drawPoint(getRotatePointX(rotateAngle, 80 + oriTextMargin, height / 2), getRotatePointY(rotateAngle, 80 + oriTextMargin, height / 2), mTmpPaint);
+        }
     }
 
     //坐标旋转公式
